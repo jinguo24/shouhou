d08f2d0fb17ceb5841f7b5236efcf53234933a4f
zhengfy <zhengfy@gmail.com>
2018-11-14 12:17:11 +0800

    update

--- html/static/initdata.js
+++ html/static/initdata.js
@@ -1,39 +1,53 @@
 var shouhouTypeMap={};
-var shouhouTypeResultStr="";
+var shouhouTypeResultJson;
 
 $(document).ready(function(){
 	initData();
 });
 function initData() {
-	//$.ajax({url:"/shouhouType/list?pageNum=1&pageSize=100",success:function(result){
-		var result = "{\"code\":200,\"message\":\"success\",\"data\":{\"pageNum\":1,\"pageSize\":100,\"size\":14,\"startRow\":1,\"endRow\":14,\"total\":14,\"pages\":1,\"list\":[{\"id\":\"1\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"赔付\",\"value\":\"pf\",\"status\":1},{\"id\":\"10\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-布丁\",\"value\":\"tb-bd\",\"status\":1},{\"id\":\"11\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"仓库发错补发\",\"value\":\"ckfc\",\"status\":1},{\"id\":\"12\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"厂家包错补发\",\"value\":\"cjbc\",\"status\":1},{\"id\":\"13\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"质量问题\",\"value\":\"zlwt\",\"status\":1},{\"id\":\"14\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"漏发补发\",\"value\":\"lf\",\"status\":1},{\"id\":\"2\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"仅退款\",\"value\":\"jtk\",\"status\":1},{\"id\":\"3\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"退货退款\",\"value\":\"thtk\",\"status\":1},{\"id\":\"4\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"物流催问\",\"value\":\"wlcw\",\"status\":1},{\"id\":\"5\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-lqyyfs\",\"value\":\"tm-lqy\",\"status\":1},{\"id\":\"6\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-朵卡连\",\"value\":\"tm-dkl\",\"status\":1},{\"id\":\"7\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-嘟嘟\",\"value\":\"pdd-dd\",\"status\":1},{\"id\":\"8\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-悠悠\",\"value\":\"pdd-yy\",\"status\":1},{\"id\":\"9\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-伊甸园\",\"value\":\"tb-ydy\",\"status\":1}],\"prePage\":0,\"nextPage\":0,\"isFirstPage\":true,\"isLastPage\":true,\"hasPreviousPage\":false,\"hasNextPage\":false,\"navigatePages\":8,\"navigatepageNums\":[1],\"navigateFirstPage\":1,\"navigateLastPage\":1,\"firstPage\":1,\"lastPage\":1}}";
-		var res = JSON.parse(result);
-		if(res.data && res.data.list) {
-			shouhouTypeResultStr = result;
+	$.ajax({url:"/shouhouapi/shouhouType/list?pageNum=1&pageSize=100&status=1",success:function(result){
+		//var result = "{\"code\":200,\"message\":\"success\",\"data\":{\"pageNum\":1,\"pageSize\":100,\"size\":14,\"startRow\":1,\"endRow\":14,\"total\":14,\"pages\":1,\"list\":[{\"id\":\"1\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"赔付\",\"value\":\"pf\",\"status\":1},{\"id\":\"10\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-布丁\",\"value\":\"tb-bd\",\"status\":1},{\"id\":\"11\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"仓库发错补发\",\"value\":\"ckfc\",\"status\":1},{\"id\":\"12\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"厂家包错补发\",\"value\":\"cjbc\",\"status\":1},{\"id\":\"13\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"质量问题\",\"value\":\"zlwt\",\"status\":1},{\"id\":\"14\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"漏发补发\",\"value\":\"lf\",\"status\":1},{\"id\":\"2\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"仅退款\",\"value\":\"jtk\",\"status\":1},{\"id\":\"3\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"退货退款\",\"value\":\"thtk\",\"status\":1},{\"id\":\"4\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"物流催问\",\"value\":\"wlcw\",\"status\":1},{\"id\":\"5\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-lqyyfs\",\"value\":\"tm-lqy\",\"status\":1},{\"id\":\"6\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-朵卡连\",\"value\":\"tm-dkl\",\"status\":1},{\"id\":\"7\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-嘟嘟\",\"value\":\"pdd-dd\",\"status\":1},{\"id\":\"8\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-悠悠\",\"value\":\"pdd-yy\",\"status\":1},{\"id\":\"9\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-伊甸园\",\"value\":\"tb-ydy\",\"status\":1}],\"prePage\":0,\"nextPage\":0,\"isFirstPage\":true,\"isLastPage\":true,\"hasPreviousPage\":false,\"hasNextPage\":false,\"navigatePages\":8,\"navigatepageNums\":[1],\"navigateFirstPage\":1,\"navigateLastPage\":1,\"firstPage\":1,\"lastPage\":1}}";
+		//var res = JSON.parse(result);
+		var res = result;
+		if(result&&res.data && res.data.list) {
+			shouhouTypeResultJson = result;
 			for (var i = 0 ; i < res.data.list.length; i++) {
 				var tdata = res.data.list[i];
 				shouhouTypeMap[tdata.type+"_"+tdata.value] = tdata.name;
 			}
+		}else{
+			alert("数据查询失败");
+			
 		}
-	//}});
+	}});
 	
 }
 
 function initSelect(type,objId,value) {
-	var res = JSON.parse(shouhouTypeResultStr);
-	if(res.data && res.data.list) {
-			var selecthtml = "";
-			for (var i = 0 ; i < res.data.list.length; i++) {
-				var opobj = res.data.list[i];
-				if (type == opobj.type) {
-					if(value && value == opobj.value) {
-						selecthtml = selecthtml + "<option value=\""+opobj.value+"\" selected>"+opobj.name+"</option>";
-					}else {
-						selecthtml = selecthtml + "<option value=\""+opobj.value+"\" >"+opobj.name+"</option>";
+	//var res = JSON.parse(shouhouTypeResultStr);
+	if(!shouhouTypeResultJson) {
+		$.ajax({url:"/shouhouapi/shouhouType/list?pageNum=1&pageSize=100&type="+type+"&status=1",success:function(result){
+			//var result = "{\"code\":200,\"message\":\"success\",\"data\":{\"pageNum\":1,\"pageSize\":100,\"size\":14,\"startRow\":1,\"endRow\":14,\"total\":14,\"pages\":1,\"list\":[{\"id\":\"1\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"赔付\",\"value\":\"pf\",\"status\":1},{\"id\":\"10\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-布丁\",\"value\":\"tb-bd\",\"status\":1},{\"id\":\"11\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"仓库发错补发\",\"value\":\"ckfc\",\"status\":1},{\"id\":\"12\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"厂家包错补发\",\"value\":\"cjbc\",\"status\":1},{\"id\":\"13\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"质量问题\",\"value\":\"zlwt\",\"status\":1},{\"id\":\"14\",\"ids\":null,\"sortColumns\":null,\"type\":\"bujian\",\"name\":\"漏发补发\",\"value\":\"lf\",\"status\":1},{\"id\":\"2\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"仅退款\",\"value\":\"jtk\",\"status\":1},{\"id\":\"3\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"退货退款\",\"value\":\"thtk\",\"status\":1},{\"id\":\"4\",\"ids\":null,\"sortColumns\":null,\"type\":\"et\",\"name\":\"物流催问\",\"value\":\"wlcw\",\"status\":1},{\"id\":\"5\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-lqyyfs\",\"value\":\"tm-lqy\",\"status\":1},{\"id\":\"6\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"天猫-朵卡连\",\"value\":\"tm-dkl\",\"status\":1},{\"id\":\"7\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-嘟嘟\",\"value\":\"pdd-dd\",\"status\":1},{\"id\":\"8\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"拼多多-悠悠\",\"value\":\"pdd-yy\",\"status\":1},{\"id\":\"9\",\"ids\":null,\"sortColumns\":null,\"type\":\"shop\",\"name\":\"淘宝-伊甸园\",\"value\":\"tb-ydy\",\"status\":1}],\"prePage\":0,\"nextPage\":0,\"isFirstPage\":true,\"isLastPage\":true,\"hasPreviousPage\":false,\"hasNextPage\":false,\"navigatePages\":8,\"navigatepageNums\":[1],\"navigateFirstPage\":1,\"navigateLastPage\":1,\"firstPage\":1,\"lastPage\":1}}";
+			//var res = JSON.parse(result);
+			var res = result;
+			if(result&&res.data && res.data.list) {
+				var selecthtml = "";
+				for (var i = 0 ; i < res.data.list.length; i++) {
+					var opobj = res.data.list[i];
+					if (type == opobj.type) {
+						if(value && value == opobj.value) {
+							selecthtml = selecthtml + "<option value=\""+opobj.value+"\" selected>"+opobj.name+"</option>";
+						}else {
+							selecthtml = selecthtml + "<option value=\""+opobj.value+"\" >"+opobj.name+"</option>";
+						}
 					}
-				}
-			}	
-			$("#"+objId).html(selecthtml);
+				}	
+				$("#"+objId).html(selecthtml);
+			}else{
+				alert("数据查询失败");
+				
+			}
+	    }});
 	}
 }
 
