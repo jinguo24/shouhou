923e97ef002e9110b557416ebaf4bf78e9003d3c
zhengfy <zhengfy@gmail.com>
2019-07-11 16:08:18 +0800

    update

--- shouhouService/src/main/java/com/simple/mapper/AnalysisWordDataMapper.java
+++ shouhouService/src/main/java/com/simple/mapper/AnalysisWordDataMapper.java
@@ -2,10 +2,13 @@ package com.simple.mapper;
 
 import java.util.*;
 import com.simple.common.crud.CommonMapper;
+import com.simple.domain.po.AnalysisWordData;
+
 import org.apache.ibatis.annotations.Param;
+
+import com.github.pagehelper.PageInfo;
 import com.simple.annotation.HoldBegin;
 import com.simple.annotation.HoldEnd;
-import com.simple.domain.po.AnalysisWordData;
 
 public interface AnalysisWordDataMapper extends CommonMapper<AnalysisWordData, String> {
 
